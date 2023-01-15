50beebb367f5bbe3d63e82434f5c62898726b58f
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2023-01-15 15:00:12 +0100

    Update tailwind.config.js

--- demo/theme/static_src/tailwind.config.js
+++ demo/theme/static_src/tailwind.config.js
@@ -6,49 +6,28 @@
  */
 
 module.exports = {
-    content: [
-        /**
-         * HTML. Paths to Django template files that will contain Tailwind CSS classes.
-         */
-        
+    mode:'jit',
+
+    content: [    
         '../../src/**/*.html',
-
-        /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
         '../templates/**/*.html',
-
-        /*
-         * Main templates directory of the project (BASE_DIR/templates).
-         * Adjust the following line to match your project structure.
-         */
         '../../templates/**/*.html',
-
-        /*
-         * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
-         * Adjust the following line to match your project structure.
-         */
         '../../**/templates/**/*.html',
-
-        /**
-         * JS: If you use Tailwind CSS in JavaScript, uncomment the following lines and make sure
-         * patterns match your project structure.
-         */
-        /* JS 1: Ignore any JavaScript in node_modules folder. */
-        // '!../../**/node_modules',
-        /* JS 2: Process all JavaScript files in the project. */
-        // '../../**/*.js',
-
-        /**
-         * Python: If you use Tailwind CSS classes in Python, uncomment the following line
-         * and make sure the pattern below matches your project structure.
-         */
-        // '../../**/*.py'
     ],
     theme: {
         extend: {
-            animation: {
-                'spin-slow': 'spin 3s linear infinite',
-                'spin-fast': 'spin 1s linear infinite',
-              }
+    // that is animation class
+      animation: {
+        fade: 'fadeOut 5s ease-in-out',
+      },
+
+      // that is actual animation
+      keyframes: theme => ({
+        fadeOut: {
+          '0%': { backgroundColor: theme('colors.red.300') },
+          '100%': { backgroundColor: theme('colors.transparent') },
+        },
+      }),
         },
         rotate:{
             '-1':'-1deg',
